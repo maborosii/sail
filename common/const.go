a33f88e51ae5bf8e2b7b3b3298dbfc830a3c3019
maborosii <bonvovageonepiece@gmail.com>
2022-06-24 14:51:54 +0800

    modify common message

--- common/const.go
+++ common/const.go
@@ -1,22 +1,7 @@
 package common
 
-import "errors"
-
-type MessageStatus int8
-type harborMessageType string
-
 const (
 	NOT_SET MessageStatus = iota
 	RECEIVED
 	DEPLOYED
-)
-
-const (
-	REPLICATION  harborMessageType = "REPLICATION"
-	UPLOAD_CHART harborMessageType = "UPLOAD_CHART"
-)
-
-var (
-	Err_NOT_SUPPORT_MSG_TYPE = errors.New("not support message type")
-	Err_NOT_SET_MSG_STATUS   = errors.New("message type not set")
 )
--- common/harbormessage.go
+++ common/harbormessage.go
@@ -1,70 +1,8 @@
 package common
 
+type MessageStatus int8
 type HarborMessage struct {
-	Type      string        `json:"type"`
-	OccurAt   int           `json:"occur_at"`
-	Operator  string        `json:"operator"`
-	EventData EventData     `json:"event_data"`
-	Status    MessageStatus `json:"status,omitempty"`
-}
-type EventData struct {
-	Replication Replication `json:"replication,omitempty"`
-	Repository  Repository  `json:"repository,omitempty"`
-	Resources   Resources   `json:"resources,omitempty"`
-}
-
-type Repository struct {
-	Name         string `json:"name"`
-	Namespace    string `json:"namespace"`
-	RepoFullName string `json:"repo_full_name"`
-	RepoType     string `json:"repo_type"`
-}
-type Resources struct {
-	Resources []Resource `json:"resources"`
-}
-type Resource struct {
-	Tag         string `json:"tag"`
-	ResourceURL string `json:"resource_url"`
-}
-
-type Replication struct {
-	HarborHostname     string               `json:"harbor_hostname"`
-	JobStatus          string               `json:"job_status"`
-	ArtifactType       string               `json:"artifact_type"`
-	AuthenticationType string               `json:"authentication_type"`
-	OverrideMode       bool                 `json:"override_mode"`
-	TriggerType        string               `json:"trigger_type"`
-	ExecutionTimestamp int                  `json:"execution_timestamp"`
-	SrcResource        SrcResource          `json:"src_resource"`
-	DestResource       DestResource         `json:"dest_resource"`
-	SuccessfulArtifact []SuccessfulArtifact `json:"successful_artifact"`
-}
-type SrcResource struct {
-	RegistryType string `json:"registry_type"`
-	Endpoint     string `json:"endpoint"`
-	Namespace    string `json:"namespace"`
-}
-type DestResource struct {
-	RegistryName string `json:"registry_name"`
-	RegistryType string `json:"registry_type"`
-	Endpoint     string `json:"endpoint"`
-	Namespace    string `json:"namespace"`
-}
-type SuccessfulArtifact struct {
-	Type    string `json:"type"`
-	Status  string `json:"status"`
-	NameTag string `json:"name_tag"`
-}
-
-func (h *HarborMessage) messageType() harborMessageType {
-	switch h.Type {
-	case string(REPLICATION):
-		return REPLICATION
-	case string(UPLOAD_CHART):
-		return UPLOAD_CHART
-	default:
-		return "unknown message type"
-	}
+	Status MessageStatus `json:"status,omitempty"`
 }
 
 // initial harbor message type
@@ -73,12 +11,12 @@ func (h *HarborMessage) InitStatus() {
 }
 
 // get harbor message type
-func (h *HarborMessage) GetStatus() (MessageStatus, error) {
+func (h *HarborMessage) GetStatus() MessageStatus {
 	// if harbor message status is not set
 	if h.Status == NOT_SET {
-		return NOT_SET, Err_NOT_SET_MSG_STATUS
+		return NOT_SET
 	}
-	return h.Status, nil
+	return h.Status
 }
 
 // transform harbor message type, example: RECEIVED -> DEPLOYED
@@ -89,15 +27,11 @@ func (h *HarborMessage) ConvertStatus(destStatus MessageStatus) {
 }
 
 func (h *HarborMessage) GetName() (string, error) {
-	if h.messageType() != UPLOAD_CHART {
-		return "", Err_NOT_SUPPORT_MSG_TYPE
-	}
-	return h.EventData.Repository.Name, nil
+	// TODO
+	return "", nil
 }
 
 func (h *HarborMessage) GetResource() (string, error) {
-	if h.messageType() != UPLOAD_CHART {
-		return "", Err_NOT_SUPPORT_MSG_TYPE
-	}
-	return h.EventData.Repository.RepoFullName, nil
+	// TODO
+	return "", nil
 }
